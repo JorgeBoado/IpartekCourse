9d0d6c4426e1f3d1d5e6b74513ecb7c48b013431
Jorge <jorge.boadogo@elorrieta-errekamari.com>
2020-10-29 14:46:12 +0100

    BBDD SQLite connection test.

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,6 +1,26 @@
 package test;
 
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+//        Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/sqlite.db");
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:G:/CursoIparTek/BBDD/sqlite.db";
+            Connection conn = DriverManager.getConnection(dbURL);
+            if (conn != null) {
+                System.out.println("Connected to the database");
+                DatabaseMetaData dm = conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+                conn.close();
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
